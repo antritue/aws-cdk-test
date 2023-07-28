f75c7031db765c9e0416f053b6f1287f1ae88262
antritue <antritue@gmail.com>
2023-07-28 17:01:25 +0700

    add create functionality

--- lib/lambdas/blog-post-handler.ts
+++ lib/lambdas/blog-post-handler.ts
@@ -1,6 +1,22 @@
-export const createBlogPostHandler = async () => {
+import { APIGatewayEvent } from "aws-lambda";
+import { BlogPost } from "./BlogPost";
+import * as crypto from "crypto";
+
+export const createBlogPostHandler = async (event: APIGatewayEvent) => {
+  const partialBlogPost = JSON.parse(event.body!) as {
+    title: string;
+    author: string;
+    content: string;
+  };
+
+  const blogPost: BlogPost = {
+    id: crypto.randomUUID(),
+    ...partialBlogPost,
+    createAt: new Date().toISOString(),
+  };
+
   return {
     statusCode: 201,
-    body: JSON.stringify({ message: "hello world 2" }),
+    body: JSON.stringify(blogPost),
   };
 };
